2532831f15a10b6e4b84b9f85f3194ac8fb86ec8
mohangrg2018 <mohangrg2030@gmail.com>
2023-05-14 13:43:43 +0545

    added about component

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,7 +1,58 @@
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="max-w-sm sm:max-w-6xl mx-auto space-y-5">
+      <h1 className="text-3xl text-amber-600 mb-9">About</h1>
+      <p>
+        IMDb, short for the Internet Movie Database, is one of the most popular
+        and widely used online movie databases. It is an extensive collection of
+        information related to movies, TV shows, actors, directors, and other
+        industry professionals. IMDb serves as a go-to resource for film
+        enthusiasts, industry insiders, and casual viewers alike.
+      </p>
+      <p>
+        IMDb was founded in 1990 and acquired by Amazon.com in 1998. It has
+        since grown into a comprehensive database with a vast collection of
+        movie-related information.Here are some key features and components of
+        IMDb: Film and TV Show Entries: IMDb provides detailed entries for
+        movies and TV shows. Each entry includes essential information such as
+        the title, release year, genre, plot summary, runtime, and production
+        details. Users can also find additional information like trivia, quotes,
+        goofs, and user reviews. Cast and Crew: IMDb lists extensive information
+        about actors, directors, writers, producers, and other crew members.
+        Users can explore the filmography of their favorite performers, learn
+        about their biographies, and discover their contributions to various
+        projects. Ratings and Reviews: IMDb allows users to rate movies and TV
+        shows on a scale of 1 to 10. These user ratings contribute to the
+        overall IMDb rating, providing an indication of the general reception of
+        a film or show. Users can also write reviews to express their opinions
+        and discuss their thoughts on specific titles. Awards and Nominations:
+        IMDb provides information about awards and nominations received by
+        movies, TV shows, actors, and other industry professionals. This
+        includes prestigious awards such as the Oscars, Golden Globes, and
+        Emmys, as well as lesser-known accolades. Box Office Data: IMDb offers
+        box office information for movies, including domestic and international
+        gross earnings. This feature allows users to track the financial success
+        of films and compare their performance with others. News and Updates:
+        IMDb provides news articles, interviews, and updates about the film
+        industry. Users can stay informed about upcoming releases, casting
+        announcements, trailers, and other relevant news through the IMDb
+        website or its mobile app. Recommendations: Based on user preferences
+        and viewing history, IMDb offers personalized movie recommendations.
+        These recommendations suggest titles that users may enjoy based on their
+        previous interactions with the platform.
+      </p>
+      <p>
+        IMDb has become an indispensable resource for movie lovers,
+        professionals, and researchers due to its extensive database,
+        user-generated content, and comprehensive coverage of the film industry.
+        It continues to be a popular go-to destination for those seeking
+        information, ratings, reviews, and insights into the world of movies and
+        television.
+      </p>
+    </div>
+  );
 };
 
 export default page;
